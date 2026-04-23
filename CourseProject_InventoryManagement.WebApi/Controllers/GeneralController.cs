7a2a62334d806b841c1e7474cb58f8236e73348a
ebrar <ebrar@LENOVO>
2026-04-24 00:12:19 +0500

    Added WebApi project.

---
+++ CourseProject_InventoryManagement.WebApi/Controllers/GeneralController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CourseProject_InventoryManagement.WebApi.Controllers
+{
+    public class GeneralController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
